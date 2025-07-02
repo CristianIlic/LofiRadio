409cd55b8309f40931dcebe871ceaa465699dbd7
CristianIlic <cris.ilic96@gmail.com>
2025-07-02 14:51:23 -0400

    feat(LiveBadge, RadioCard): badge for lives and statistic for pre-recorded videos

---
+++ src/components/RadioCard/LiveBadge.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LiveBadge() {
+  return (
+    <div id="live-badge">
+      <div id="red-circle" />
+      <div>LIVE</div>
+    </div>
+  );
+}
+
+export default LiveBadge;
--- src/components/RadioCard/index.jsx
+++ src/components/RadioCard/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { formatNumber, truncateText } from "../../utils/functions";
 import useIsMobile from "../../hooks/useIsMobile";
+import LiveBadge from "./LiveBadge";
 
 function RadioCard({
   title,
@@ -13,14 +14,16 @@ function RadioCard({
 }) {
   const isMobile = useIsMobile();
   const truncTitle = truncateText(title, isMobile ? 18 : 30);
-
   return (
     <div className="cardContainer" {...props}>
-      <img className={`${className}`} src={thumbnail} alt="" />
+      <div className="imageContainer">
+        <img className={`${className}`} src={thumbnail} alt="" />
+        {viewers !== 0 ? <LiveBadge /> : ""}
+      </div>
       <div className="radioInfo">
         <div className="topInfo">
           <h1>{truncTitle}</h1>
-          <p>{formatNumber(viewers)} listening</p>
+          <p>{viewers !== 0 ? `${formatNumber(viewers)} listening` : ""}</p>
         </div>
         <div className="bottomInfo">
           <h3>{channel}</h3>
